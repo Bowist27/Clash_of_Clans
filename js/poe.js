bc75eabc19b38168e3c0d187e5bd036cd4c5668a
Tony <111481792+Bowist27@users.noreply.github.com>
2024-03-12 00:48:36 -0600

    Subir nivel al barbaro

--- js/poe.js
+++ js/poe.js
@@ -1,4 +1,19 @@
+const barbaro = {nivel: 1, vida: 20, ataque: 12};
+
 const boton_levelear_barbaro = document.getElementById("boton_levelear_barbaro");
+
+const subir_barbaro = () => {
+    barbaro.nivel++;
+    barbaro.vida += 4;
+    barbaro.ataque += 2;
+}
+
 boton_levelear_barbaro.onclick = () => {
-    console.log("click");
+    subir_barbaro();
+    const span_nivel = document.getElementById("nivel_barbaro");
+    span_nivel.innerHTML = barbaro.nivel;
+    const span_ataque = document.getElementById("ataque_barbaro");
+    span_ataque.innerHTML = barbaro.ataque;
+    const span_vida = document.getElementById("vida_barbaro");
+    span_vida.innerHTML = barbaro.vida;
 }

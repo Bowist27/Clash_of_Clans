e1bc9ac68b339f3595932516a4999ec8c14ad1fd
Tony <111481792+Bowist27@users.noreply.github.com>
2024-03-12 03:21:47 -0600

    Ejemplos codigo asincrono

--- app.js
+++ app.js
@@ -5,25 +5,24 @@ const filesystem = require('fs');
 //Se escribe el segundo parámetro en el archivo del primer parámetro
 filesystem.writeFileSync('hola.txt', 'Hola mundo desde node');
 
+const te_hackie = () => {
+    console.log("jojo te hackié");
+}
+
+//setTimeout ejecuta la función recibida como primer parámetro 
+//cuando hayan transcurrido los milisegundos del segunto parámetro
+setTimeout(te_hackie, 7000);
 
 const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
 
+//Imprime el arreglo de manera ordenada
 for (let item of arreglo) {
     setTimeout(() => {
         console.log(item);
     }, item);
 } 
 
-
-
-
-
-
-
-
-
-
-
+console.log("¿En dónde se ejecuta esta línea?");
 
 
 // const http = require('http');
